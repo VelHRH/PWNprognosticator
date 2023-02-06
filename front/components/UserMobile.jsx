8fde8298c846ef102c49d4faf2e86e2f1007fb7c
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-02-06 19:33:21 +0300

    Fontawesome added

--- front/components/UserMobile.jsx
+++ front/components/UserMobile.jsx
@@ -16,9 +16,13 @@ export const UserMobile = ({
     place > 3 && place < 11 && "text-yellow-400"
    } ${
     place > 10 && place < 16 && "text-yellow-400"
-   } px-2 py-1 bg-slate-600 text-slate-50 my-2 lg:m-2 w-full rounded-lg h-8 flex justify-between`}
+   } px-2 py-1 bg-slate-600 text-slate-50 my-2 lg:m-2 w-full rounded-lg h-8 flex justify-between items-center`}
   >
-   <div>{children}</div>
+   <div className="flex items-center">
+    <div>{children}</div>
+
+    <i class="fa-solid fa-circle-info ml-3"></i>
+   </div>
    <div>{total}</div>
   </button>
  );
