a5f7014d409a991a4d29ecea72bfdef5f9a741a3
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-02-06 16:28:48 +0300

    Added colors for top 15

--- front/components/User.jsx
+++ front/components/User.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export const User = ({ children, place }) => {
  return (
   <div
-   className={`${place === 1 && "bg-amber-400 text-slate-50"} ${
-    place === 2 && "bg-slate-400 text-slate-50"
-   } ${place === 3 && "bg-orange-700 text-slate-50"} ${
-    place > 3 && place < 11 && "bg-teal-300 text-slate-50"
+   className={`${place === 1 && "text-yellow-400"} ${
+    place === 2 && "text-yellow-400"
+   } ${place === 3 && "text-yellow-400"} ${
+    place > 3 && place < 11 && "text-yellow-400"
    } ${
-    place > 10 && place < 16 && "bg-purple-400 text-slate-50"
+    place > 10 && place < 16 && "text-yellow-400"
    } px-2 py-1 bg-slate-600 text-slate-50 my-2 lg:m-2 w-full rounded-lg h-8 flex justify-between`}
   >
    <div>{children}</div>
