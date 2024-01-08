7c5b85a8ce5bc50b0016d5e3ed9ea673bff9fcef
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2024-01-08 14:58:55 +0300

    simpler logic

--- back/src/index.js
+++ back/src/index.js
@@ -1,15 +1,15 @@
-import express from "express"
-import { config } from "dotenv"
-import cors from "cors"
-import mongoose from "mongoose"
-import Year2023Model from "./models/Year2023.js"
-import Year2022Model from "./models/Year2022.js"
+import express from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import Year2023Model from './models/Year2023.js';
 
-config()
+config();
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("DB OK"))
-.catch((err) => console.log("DB Error", err))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DB OK'))
+  .catch(err => console.log('DB Error', err));
 
 const app = express();
 
@@ -19,144 +19,77 @@ app.use(cors());
 function sumArray(array) {
   let sum = 0;
   for (const item of array) {
-   sum += item.points;
+    sum += item.points;
   }
   return sum;
- }
+}
 
-app.get("/2023", async (req, res) => {
-  try{
-    const r = await Year2023Model.find();
-    for (let i=0; i< r.length; i++) {
-      for (let j=1; j< r[i].results.length; j++) {
-        if (r[i].results[j].show == r[i].results[j-1].show){
-          r[i].results.splice(j-1, 1);
+app.get('/:year', async (req, res) => {
+  try {
+    const documents = await Year2023Model.find({ year: req.params.year });
+    for (let i = 1; i < documents.length; i++) {
+      for (let j = 0; j < documents.length - i; j++) {
+        if (sumArray(documents[j].results) < sumArray(documents[j + 1].results)) {
+          [documents[j], documents[j + 1]] = [documents[j + 1], documents[j]];
         }
       }
     }
-    for (let i = 1; i < r.length; i++) {
-      for (let j = 0; j < r.length - i; j++) {
-        if (sumArray(r[j].results) < sumArray(r[j+1].results)) {
-          [r[j], r[j + 1]] = [r[j + 1], r[j]];
-        }
-      }
-    }
-    return res.json(r);
-  }
-  catch (err) {
+    return res.json(documents);
+  } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Unable to get data" });
-   }
-})
+    res.status(500).json({ message: 'Unable to get data' });
+  }
+});
 
-app.post("/2023", async (req, res) => {
-  try{
-    config()
-    if (req.body.password !== process.env.SECRET) return res.status(403).json({message: "fail"})
+app.post('/', async (req, res) => {
+  try {
+    const currentYear = new Date().getFullYear();
+    if (req.body.password !== process.env.SECRET)
+      return res.status(403).json({ message: 'not allowed' });
     const data = await req.body.data;
     let ret = [];
-    for (let i in data){
-      const user = await data[i].slice(0, data[i].indexOf(" -"));
-      const userData = await Year2023Model.find(
-        {user: user},
-      )
+    for (let i in data) {
+      const user = await data[i].slice(0, data[i].indexOf(' -'));
+      const userData = await Year2023Model.find({ user: user, year: currentYear });
       if (userData.length === 0) {
         const doc = new Year2023Model({
           user: user,
-          results: [{
-            show: req.body.show,
-            points: parseFloat(data[i].slice(data[i].indexOf(" - ")+3))
-        }]
-        })
+          year: currentYear,
+          results: [
+            {
+              show: req.body.show,
+              points: parseFloat(data[i].slice(data[i].indexOf(' - ') + 3)),
+            },
+          ],
+        });
         const r = await doc.save();
         ret.push(r);
       } else {
         const r = await Year2023Model.findOneAndUpdate(
-          {user: user},
+          { user: user, year: currentYear },
           {
-            $push: {results: {show: req.body.show, points: parseFloat(data[i].slice(data[i].indexOf(" - ")+3))}}
+            $push: {
+              results: {
+                show: req.body.show,
+                points: parseFloat(data[i].slice(data[i].indexOf(' - ') + 3)),
+              },
+            },
           },
-          {returnDocument: "after"}
+          { returnDocument: 'after' },
         );
         ret.push(r);
       }
     }
     return res.json(ret);
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Unable to post data" });
-   }
-})
-
-app.get("/2022", async (req, res) => {
-  try{
-    const r = await Year2022Model.find();
-    for (let i=0; i< r.length; i++) {
-      for (let j=1; j< r[i].results.length; j++) {
-        if (r[i].results[j].show == r[i].results[j-1].show){
-          r[i].results.splice(j-1, 1);
-        }
-      }
-    }
-    for (let i = 1; i < r.length; i++) {
-      for (let j = 0; j < r.length - i; j++) {
-        if (sumArray(r[j].results) < sumArray(r[j+1].results)) {
-          [r[j], r[j + 1]] = [r[j + 1], r[j]];
-        }
-      }
-    }
-    return res.json(r);
+    res.status(500).json({ message: 'Unable to post data' });
   }
-  catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Unable to get data" });
-   }
-})
+});
 
-app.post("/2022", async (req, res) => {
-  try{
-    const data = await req.body.data;
-    let ret = [];
-    for (let i in data){
-      const user = await data[i].slice(0, data[i].indexOf(" -"));
-      const userData = await Year2022Model.find(
-        {user: user},
-      )
-      if (userData.length === 0) {
-        const doc = new Year2022Model({
-          user: user,
-          results: [{
-            show: req.body.show,
-            points: parseFloat(data[i].slice(data[i].indexOf(" - ")+3))
-        }]
-        })
-        const r = await doc.save();
-        ret.push(r);
-      } else {
-        const r = await Year2022Model.findOneAndUpdate(
-          {user: user},
-          {
-            $push: {results: {show: req.body.show, points: parseFloat(data[i].slice(data[i].indexOf(" - ")+3))}}
-          },
-          {returnDocument: "after"}
-        );
-        ret.push(r);
-      }
-    }
-    return res.json(ret);
+app.listen(process.env.PORT || 4445, err => {
+  if (err) {
+    return console.log(err);
   }
-  catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Unable to post data" });
-   }
-})
-
-app.listen(process.env.PORT || 4445, (err) => 
-  {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("Server OK");
-  }
-);
+  console.log('Server OK');
+});
--- back/src/models/Year2023.js
+++ back/src/models/Year2023.js
@@ -1,18 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const Year2023Schema = new mongoose.Schema({
-  user: {
-    type: String,
-    required: true,
+const Year2023Schema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+      required: true,
+    },
+    results: {
+      type: Array,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
   },
-  results: {
-    type: Array,
-    required: true,
+  {
+    timestamps: true,
   },
-},
-{
-  timestamps: true
-}
 );
 
 export default mongoose.model('Year2023', Year2023Schema);
